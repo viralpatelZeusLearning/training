7c6b3e61f968cff61358d19bf1e64d05e2376560
viralpatelZeusLearning <viral.patel@zeuslearning.com>
2024-06-28 18:30:42 +0530

    json data update

--- day4-task/home-page/index.js
+++ day4-task/home-page/index.js
@@ -58,4 +58,145 @@ smallLis.forEach(li => {
     })
 })
 
+var announceicon = document.querySelector(".announce")
+var announcementlist = document.querySelector(".anouncement")
+announceicon.addEventListener("click",()=>{
+    announcementlist.dataset["state"] = announcementlist.dataset["state"]!=="clicked" ? "clicked" : "closed"
+})
+announceicon.addEventListener("mouseover" ,()=>{
+    if(announcementlist.dataset["state"]==="closed"){
+        announcementlist.dataset["state"]="open"
+    }
+})
+announceicon.addEventListener("mouseleave",()=>{
+    if(announcementlist.dataset["state"]==="open"){
+        announcementlist.dataset["state"]="closed"
+    }
+})
+var notifyicon = document.querySelector(".notifyicon")
+var notifylist = document.querySelector(".notify")
+notifyicon.addEventListener("click",()=>{
+    notifylist.dataset["state"] = announcementlist.dataset["state"]!=="clicked" ? "clicked" : "closed"
+})
+notifyicon.addEventListener("mouseover" ,()=>{
+    if(notifylist.dataset["state"]==="closed"){
+        notifylist.dataset["state"]="open"
+    }
+})
+notifyicon.addEventListener("mouseleave",()=>{
+    if(notifylist.dataset["state"]==="open"){
+        notifylist.dataset["state"]="closed"
+    }
+})
 
+import c from './data.json' with {type:"json"}
+console.log(c);
+function createCourse(c){
+    // let card = `<div class="card" ${c.favourite ? "data-starred" : ""} ${c.preview ? "data-visible" : ""} ${c.manage ? "data-calendar" : ""} ${c.grade : "data-shop" : ""}  ${ c.report ? "data-chart" : ""}>`;
+    let contentCard = document.createElement("div");
+    contentCard.classList.add("content-card")
+    // if(c.preview){contentCard.dataset["visible"]=true}
+    // if(c.manage){contentCard.dataset["calendar"]=true}
+    // if(c.grade){contentCard.dataset["shop"]=true}
+    // if(c.report){contentCard.dataset["chart"]=true}
+  
+    let card = document.createElement("div");
+    card.classList.add("card")
+  
+    let imgDiv = document.createElement("div");
+    imgDiv.classList.add("image")
+    let img = document.createElement("img")
+    img.src = c.courseImg
+    // img.alt = c.courseTitle
+    imgDiv.appendChild(img)
+    card.appendChild(imgDiv)
+  
+    let content = document.createElement("div")
+    content.classList.add("content")
+  
+    let cardContent = document.createElement("div")
+    cardContent.classList.add("card-content-head")
+    if(c.favourite){cardContent.dataset["starred"]=true}
+    let h = document.createElement("h3")
+    h.textContent = c.courseTitle
+    cardContent.appendChild(h)
+   
+    let starSpan = document.createElement("span")
+    starSpan.classList.add("material-symbols-outlined")
+    starSpan.textContent=("star")
+    cardContent.appendChild(starSpan)
+    content.appendChild(cardContent)
+  
+    let info = document.createElement("div")
+    info.classList.add("info")
+    let subjectGradeSpan = document.createElement("span")
+    
+    info.innerHTML = `<span>${c.subject}</span><span>|</span><span>Grade ${c.classGrade} <span>+${c.specialGrade}</span></span>`
+    
+  content.appendChild(info)
+  
+  let infoB = document.createElement("div")
+  infoB.classList.add("info")
+  if(c.units || c.lessons || c.topics){
+    infoB.innerHTML=`
+                            <span class="no">${c.units}</span>
+                            <span>Units</span>
+                            <span class="no">${c.lessons} </span>
+                            <span>Lessons</span>
+                            <span class="no">${c.topics} </span>
+                            <span>Topics</span>
+                        `
+  }
+   content.appendChild(infoB)
+    let selection = document.createElement("select")
+    selection.name = "class"
+    selection.classList.add("class-selection")
+    if(c.class){
+      selection.innerHTML = `<option value="${c.class}">${c.class}</option>`
+    }
+    else{
+      selection.innerHTML = `<option value="">No Classes</option>`
+    }
+    content.appendChild(selection)
+    content.appendChild(document.createElement("br"))
+   
+    if(c.numberOfStudents || c.time){
+      let studentsSchedule = document.createElement("span")
+      // studentsSchedule.classList.add("students-schedule")
+      if(c.numberOfStudents){studentsSchedule.innerHTML = studentsSchedule.innerHTML + `${c.numberOfStudents} students`}
+    //   if(c.numberOfStudents && c.time){studentsSchedule.innerHTML = studentsSchedule.innerHTML + `<span>&#124;</span>`}
+      if(c.time){studentsSchedule.innerHTML = studentsSchedule.innerHTML + `${c.time.startDate} - ${c.time.endDate}`}
+     
+      content.appendChild(studentsSchedule)
+    }
+    card.appendChild(content)
+    
+    let interactables = document.createElement("div")
+    interactables.classList.add("content-icons")
+    interactables.innerHTML = `<span class="material-symbols-outlined">
+                        <img src="../home-page/icons/icons/preview.svg">
+                    </span>
+                    <span class="material-symbols-outlined" data-unstared="">
+                        <img src="../home-page/icons/icons/manage course.svg">
+                    </span>
+                    <span class="material-symbols-outlined" data-unstared="">
+                        <img src="../home-page/icons/icons/grade submissions.svg">
+                    </span>
+                    <span class="material-symbols-outlined chart-icon">
+                        <img src="../home-page/icons/icons/reports.svg">
+                    </span>`
+    if(c.preview){contentCard.dataset["visible"]=true}
+    if(c.manage){contentCard.dataset["calendar"]=true}
+    if(c.grade){contentCard.dataset["shop"]=true}
+    if(c.report){contentCard.dataset["chart"]=true}
+    contentCard.appendChild(card)
+    contentCard.appendChild(interactables)
+   return contentCard;
+  }
+  c.course.forEach( (x)=>{
+    // console.log(x);
+    document.querySelector("main").appendChild(createCourse(x))
+  })
+//   document.body.appendChild(createCourse(c.course[0]));
+// document.body.appendChild(createCourse(course));
+// console.log(createCourse(course))
